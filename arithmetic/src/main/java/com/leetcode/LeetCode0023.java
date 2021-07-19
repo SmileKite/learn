74b6779031ccb8ebe3c2f46e2499ff6c5a26285f
SmileKite <17621792504@163.com>
2021-07-19 20:51:18 +0800

    Revert "Revert "解释器模式""
    This reverts commit 03d6d82b

---
+++ arithmetic/src/main/java/com/leetcode/LeetCode0023.java
@@ -0,0 +1,17 @@
+package com.leetcode;
+
+import com.leetcode.entity.ListNode;
+
+/**
+ * 0023--[合并K个升序链表]
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ * @author lizhenyu
+ * @date 2021/7/10
+ */
+public class LeetCode0023 {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        return null;
+    }
+}
---
+++ arithmetic/src/test/java/com/leetcode/LeetCode0023Test.java
@@ -0,0 +1,8 @@
+package com.leetcode;
+
+/**
+ * @author lizhenyu
+ * @date 2021/7/10
+ */
+public class LeetCode0023Test {
+}
