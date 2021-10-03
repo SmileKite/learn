b52f404f9e7c7c92b232493f7f9554e8a54e7fcb
SmileKite <17621792504@163.com>
2021-10-03 08:01:35 +0800

    166-分数到小数

---
+++ arithmetic/src/main/java/com/leetcode/LeetCode0166.java
@@ -0,0 +1,62 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 0166-[分数到小数]
+ * 给定两个整数，分别表示分数的分子 numerator 和分母 denominator，以 字符串形式返回小数 。
+ * 如果小数部分为循环小数，则将循环的部分括在括号内。
+ * 如果存在多个答案，只需返回 任意一个 。
+ * 对于所有给定的输入，保证 答案字符串的长度小于 10^4 。
+ * @author lizhenyu
+ * @date 2021/10/3
+ */
+public class LeetCode0166 {
+
+    /**
+     * 分数转小数
+     * @param numerator 被除数
+     * @param denominator 除数
+     * @return 结果
+     */
+    public String fractionToDecimal(int numerator, int denominator) {
+        long numeratorLong = (long) numerator;
+        long denominatorLong = (long) denominator;
+        if (numeratorLong % denominatorLong == 0) {
+            return String.valueOf(numeratorLong / denominatorLong);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        if (numeratorLong < 0 ^ denominatorLong < 0) {
+            sb.append('-');
+        }
+
+        numeratorLong = Math.abs(numeratorLong);
+        denominatorLong = Math.abs(denominatorLong);
+        long integerPart = numeratorLong / denominatorLong;
+        sb.append(integerPart);
+        sb.append(".");
+
+        StringBuilder fractionPart = new StringBuilder();
+        Map<Long, Integer> remainderIndexMap = new HashMap<>();
+        long remainder = numeratorLong % denominatorLong;
+        int index = 0;
+        while (remainder != 0 && !remainderIndexMap.containsKey(remainder)) {
+            remainderIndexMap.put(remainder, index);
+            remainder *= 10;
+            fractionPart.append(remainder / denominatorLong);
+            remainder %= denominatorLong;
+            index++;
+        }
+        if (remainder != 0) { // 有循环节
+            int insertIndex = remainderIndexMap.get(remainder);
+            fractionPart.insert(insertIndex, '(');
+            fractionPart.append(')');
+        }
+        sb.append(fractionPart);
+
+        return sb.toString();
+    }
+
+}
---
+++ arithmetic/src/test/java/com/leetcode/LeetCode0166Test.java
@@ -0,0 +1,17 @@
+package com.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author lizhenyu
+ * @date 2021/10/3
+ */
+public class LeetCode0166Test {
+
+    @Test
+    public void test() {
+        LeetCode0166 leetCode0166 = new LeetCode0166();
+
+        System.out.println(leetCode0166.fractionToDecimal(2, 3));
+    }
+}
